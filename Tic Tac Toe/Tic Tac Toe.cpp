fc9e59984314e1c5ed65b9e388a289e4e80ed302
ashwani762 <ashwanikumar.bachchan@gmail.com>
2017-01-08 17:46:25 +0530

    Invalid move fix

--- Tic Tac Toe/Tic Tac Toe.cpp
+++ Tic Tac Toe/Tic Tac Toe.cpp
@@ -10,8 +10,7 @@ int filled = 0;
 //Functions
 void display(char[]);
 int whogoesfirst();
-void gameover();
-void checkwin();
+void checkwin(char[]);
 void player(char[], char);
 int checkgrid(char[]);
 
@@ -79,26 +78,31 @@ void player(char box[], char symbol)
 {
 	int pos;
 	//system("cls");
+again:
 	printf_s("Enter the location");
 	scanf_s("%d", &pos);
+
 	if (box[pos] == ' ')
 	{
 		if (symbol == 'O')
 		{
 			box[pos] = 'O';
+			filled++;
 		}
 		else
 		{
 			box[pos] = 'X';
+			filled++;
 		}
 	}
 	else
 	{
-		printf_s("Invalid move\n");
+		printf_s("Invalid move! Try again");
+		goto again;
 	}
-	filled++;
+	
 	display(box);
-	printf_s("%d", filled);
+	//printf_s("%d", filled);
 	
 }
 
@@ -112,4 +116,9 @@ int checkgrid(char box[])
 	{
 		return 0;
 	}
+}
+
+void checkwin(char box[])
+{
+
 }
